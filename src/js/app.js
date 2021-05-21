aaa0429de9a824f9e94fe12fb6dbf5ef2c13def8
oxenprogrammer <emmy1000okello@gmail.com>
2021-05-22 01:47:52 +0300

    Perf(config): add sass loader

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
+import '../style/main.scss';
+
 import component from './component';
 
 document.body.append(component());
--- webpack.config.js
+++ webpack.config.js
@@ -19,15 +19,28 @@ module.exports = {
     }),
   ],
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env'],
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
         },
       },
-    }],
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+        // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
   },
 };
