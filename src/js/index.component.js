53e281620c38b3630c1bdd52930bd3a5f334e598
oxenprogrammer <emmy1000okello@gmail.com>
2021-05-24 23:59:33 +0300

    Feat(menu): add menu items

--- src/js/index.component.js
+++ src/js/index.component.js
@@ -1,9 +1,11 @@
 import headerComponent from './header.component';
+import menuComponent from './menu.component';
 
 const indexComponent = () => {
   const { body } = document;
 
   body.appendChild(headerComponent());
+  body.appendChild(menuComponent());
 
   return body;
 };
