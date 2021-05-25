193f2e5cf068bbd055475d123e13c9306ec3966e
oxenprogrammer <emmy1000okello@gmail.com>
2021-05-25 22:54:51 +0300

    Feat(home): add background image

--- src/js/home.component.js
+++ src/js/home.component.js
@@ -1,10 +1,20 @@
+import home1 from '../img/home1.jpg';
+// import home2 from '../img/home2.jpg';
+// import home3 from '../img/home3.jpg';
+
 const homeComponent = () => {
   const home = document.createElement('section');
   home.setAttribute('class', 'home');
-  const h1 = document.createElement('h1');
-  h1.textContent = 'Home Page';
+  const container = document.createElement('article');
+  container.setAttribute('class', 'container');
 
-  home.appendChild(h1);
+  const img1 = document.createElement('img');
+  img1.setAttribute('class', 'home-image_1');
+  img1.setAttribute('src', home1);
+
+  container.appendChild(img1);
+
+  home.appendChild(container);
 
   return home;
 };
