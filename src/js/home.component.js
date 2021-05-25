600df56b7607cf7b1343c8dfda137ca75fe496ca
oxenprogrammer <emmy1000okello@gmail.com>
2021-05-25 23:01:35 +0300

    Feat(home): update background image

--- src/js/home.component.js
+++ src/js/home.component.js
@@ -1,6 +1,4 @@
-import home1 from '../img/home1.jpg';
-// import home2 from '../img/home2.jpg';
-// import home3 from '../img/home3.jpg';
+import home2 from '../img/home2.jpg';
 
 const homeComponent = () => {
   const home = document.createElement('section');
@@ -10,7 +8,7 @@ const homeComponent = () => {
 
   const img1 = document.createElement('img');
   img1.setAttribute('class', 'home-image_1');
-  img1.setAttribute('src', home1);
+  img1.setAttribute('src', home2);
 
   container.appendChild(img1);
 
