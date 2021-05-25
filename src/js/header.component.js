419fe88388e0d1b81ea64c423697ffd59500225d
oxenprogrammer <emmy1000okello@gmail.com>
2021-05-25 18:16:56 +0300

    Feat(header): add menu links

--- src/js/header.component.js
+++ src/js/header.component.js
@@ -4,16 +4,31 @@ const headerComponent = () => {
   const header = document.createElement('nav');
   header.setAttribute('class', 'navbar');
 
-  const title = document.createElement('h1');
-  title.setAttribute('class', 'title');
-  title.textContent = 'La Carne Restaurante';
-
   const img = document.createElement('img');
   img.setAttribute('class', 'logo');
   img.setAttribute('src', logo);
 
-  header.appendChild(title);
+  const links = document.createElement('header');
+  links.setAttribute('class', 'nav-links');
+
+  const home = document.createElement('div');
+  home.setAttribute('class', 'link home-link');
+  home.textContent = 'home';
+
+  const menu = document.createElement('div');
+  menu.setAttribute('class', 'link menu-link');
+  menu.textContent = 'menu';
+
+  const contact = document.createElement('div');
+  contact.setAttribute('class', 'link contact-link');
+  contact.textContent = 'contact';
+
+  links.appendChild(home);
+  links.appendChild(menu);
+  links.appendChild(contact);
+
   header.appendChild(img);
+  header.appendChild(links);
 
   return header;
 };
